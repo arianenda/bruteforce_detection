b2c8a77f82614d0e0c7211b079fe223e8334f8c0
arianenda <athariq@evanarianenda.com>
2025-03-07 23:22:53 +0700

    Add: log line format validation

--- internal/detection/linux_bruteforce.go
+++ internal/detection/linux_bruteforce.go
@@ -16,6 +16,7 @@ func BruteForce(file *os.File) {
 	for {
 		line, err := fileReader.ReadString('\n')
 		parseLine := parser.ParseLogLine(line)
+
 		if value, ok := linuxLog[parseLine.SrcIP]; ok {
 			value.BruteForceCount += 1
 			linuxLog[parseLine.SrcIP] = value
@@ -27,10 +28,8 @@ func BruteForce(file *os.File) {
 		}
 	}
 
-	fmt.Println("Map: ", linuxLog)
-
 	for ip, log := range linuxLog {
-		if log.BruteForceCount >= parser.MAX_COUNT {
+		if log.BruteForceCount >= parser.MAX_COUNT && log.SrcIP != "" {
 			fmt.Printf("Possible brute force attacks from IP: %s with %d login attempts", ip, log.BruteForceCount)
 		}
 	}
--- internal/parser/linux_log_parser.go
+++ internal/parser/linux_log_parser.go
@@ -1,6 +1,10 @@
 package parser
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type LinuxAuthLog struct {
 	SrcIP           string
@@ -10,18 +14,29 @@ type LinuxAuthLog struct {
 }
 
 const (
-	SSH          = "sshd"
-	FAILURE_INFO = "Failed password"
-	MAX_COUNT    = 5
+	SSH              = "sshd"
+	FAILURE_INFO     = "Failed password"
+	MAX_COUNT        = 5
+	NOT_INVALID_USER = "invalid"
 )
 
 func ParseLogLine(logline string) LinuxAuthLog {
 	logInfo := LinuxAuthLog{}
 	logLine := string(logline)
 
-	if strings.Contains(logLine, SSH) && strings.Contains(logLine, FAILURE_INFO) {
+	if strings.Contains(logLine, SSH) && strings.Contains(logLine, FAILURE_INFO) && !strings.Contains(logLine, NOT_INVALID_USER) {
 		splitAfterFor := strings.Split(logLine, "for")
+
+		if len(splitAfterFor) < 2 {
+			fmt.Printf("Unable to parsing the log")
+			os.Exit(-1)
+		}
+
 		strInfo := strings.Split(splitAfterFor[1], " ")
+		if len(strInfo) != 7 {
+			return logInfo
+		}
+
 		logInfo.SrcIP = strInfo[3]
 		logInfo.SrcPort = strInfo[5]
 		logInfo.Username = strInfo[1]
