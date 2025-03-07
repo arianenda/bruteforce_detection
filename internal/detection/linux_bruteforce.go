1f0c404e783a361ba45d39898504da320cb12b8a
arianenda <athariq@evanarianenda.com>
2025-03-07 23:29:35 +0700

    Fix: change the output format with a line break due to the possibility of multiple brute force attack events

--- internal/detection/linux_bruteforce.go
+++ internal/detection/linux_bruteforce.go
@@ -30,7 +30,7 @@ func BruteForce(file *os.File) {
 
 	for ip, log := range linuxLog {
 		if log.BruteForceCount >= parser.MAX_COUNT && log.SrcIP != "" {
-			fmt.Printf("Possible brute force attacks from IP: %s with %d login attempts", ip, log.BruteForceCount)
+			fmt.Printf("Possible brute force attacks from IP: %s with %d login attempts\n", ip, log.BruteForceCount)
 		}
 	}
 }
